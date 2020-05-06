cecb63aa8ae4dd9fa36a253386f7fe81d38ddf3e
Gabriel Martins <gabrielrms@outlook.com.br>
2020-05-06 09:04:17 -0300

    Formulário para atualizar os dados do usuário completo

--- registro-membros/src/app/components/login/usuario.model.ts
+++ registro-membros/src/app/components/login/usuario.model.ts
@@ -3,5 +3,6 @@ export interface Usuario {
     nome: string
     email: string
     senha: string
+    telefone?: string
     igreja?: string
 }
--- registro-membros/src/app/components/perfil/perfil.component.ts
+++ registro-membros/src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,8 @@
+import { Usuario } from './../login/usuario.model';
+import { Router } from '@angular/router';
+import { PerfilService } from './perfil.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  constructor(
+    private perfilService: PerfilService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.usuario = JSON.parse(localStorage.getItem('usuario'));
   }
 
+  update(): void {
+    console.log(this.usuario);
+  }
 }
