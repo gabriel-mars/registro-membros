aec07a6bb79e253132ddc440ba02ccbdf5220f70
Gabriel Martins <gabrielrms@outlook.com.br>
2020-05-31 22:39:47 -0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAb4AyqCfM5MKbnBI634NYqG5oIIbMDfdA',
+    authDomain: 'registro-membros-d3d48.firebaseapp.com',
+    databaseURL: 'https://registro-membros-d3d48.firebaseio.com',
+    projectId: 'registro-membros-d3d48',
+    storageBucket: 'registro-membros-d3d48.appspot.com',
+    messagingSenderId: '1052710370091'
+  }
 };
