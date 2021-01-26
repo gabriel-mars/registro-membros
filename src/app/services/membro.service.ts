ef65fe6a5eb109acf31e298fc05911629421c3c1
codefactor-io <support@codefactor.io>
2021-01-26 19:04:13 +0000

    [CodeFactor] Apply fixes

--- src/app/services/membro.service.ts
+++ src/app/services/membro.service.ts
@@ -2,7 +2,7 @@ import { ToastService } from './toast.service';
 import { Usuario } from '../models/usuario.model';
 import { Membro } from '../models/membro.model';
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore'
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -26,21 +26,21 @@ export class MembroService {
 
   getMembros() {
     this.usuario = JSON.parse(localStorage.getItem('usuario'));
-    let codIgreja = this.usuario.codIgreja;
+    const codIgreja = this.usuario.codIgreja;
     return this.firestore.collection('membro', ref => ref.where('igreja', '==', codIgreja)).valueChanges();
   }
 
   update(membro: Membro): void {
     this.usuario = JSON.parse(localStorage.getItem('usuario'));
-    let codIgreja = this.usuario.codIgreja;
-    
+    const codIgreja = this.usuario.codIgreja;
+
     this.firestore.collection('membro', ref => ref.where('igreja', '==', `${codIgreja}`).where('id', '==', membro.id)).get().toPromise()
     .then(snap => {
         snap.forEach(doc => {
           this.membro = doc.data() as Membro;
           this.firestore.doc(`membro/${doc.id}`).set(membro);
           this.toastService.showMessage('Membro atualizado!', true);
-        })    
+        });
     })
     .catch(err => {
       this.toastService.showMessage('Ocorreu um erro!', false);
@@ -52,9 +52,9 @@ export class MembroService {
     this.firestore.collection('membro').get().toPromise()
     .then(snap => {
         snap.forEach(doc => {
-            let aux = doc.id;
+            const aux = doc.id;
             this.membro = doc.data() as Membro;
-            
+
             if (this.membro.id == id){
               this.firestore.collection('membro').doc(`${aux}`).delete()
               .then(() => {
